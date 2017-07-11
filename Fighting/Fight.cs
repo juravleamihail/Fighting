368e5f954932f5ce3bf5e59271e9a3f85777767b
CristianBanan <cristian.ursache96@yahoo.com>
2017-07-11 10:56:50 +0300

    clasa Fight

--- Fighting/Fight.cs
+++ Fighting/Fight.cs
@@ -9,17 +9,36 @@ namespace Fighting
     class Arena
     {
         Player first = new Player();
-        Player second= new Player();
+        Player second = new Player();
+        Player Winner = new Player();
 
         public Random r = new Random();
 
-        public void Fight(Player first,Player second)
+        public string Fight(Player first, Player second)
         {
+            public int random = r.Next(0, 100);
+            while(first.hp > 0 || second.hp > 0){
+            if(random< 50){    
+
             first.hp -= (second.forta) / first.protection;
-        }
+            }
+            
 
+            else{
 
+            second.hp -= (first.forta) / second.protection;
+            }
+            if(first.hp >= 0){
+            Winner = first;
+            }
 
+            else{
+            Winner = second;
+            }
 
+}
+
+    return "Winner is" + Winner.name;
     }
+}
 }
