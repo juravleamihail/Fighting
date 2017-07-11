70231eb4e921a2d21981e11f53e4c26294098f27
iquest <iquest@SB070020PC>
2017-07-11 12:08:51 +0300

    afisare consola in program.cs

--- Fighting/Fight.cs
+++ Fighting/Fight.cs
@@ -9,36 +9,19 @@ namespace Fighting
     class Arena
     {
         Player first = new Player();
-        Player second = new Player();
-        Player Winner = new Player();
+        Player second= new Player();
 
         public Random r = new Random();
 
-        public string Fight(Player first, Player second)
+        public void Fight(Player first,Player second)
         {
-            public int random = r.Next(0, 100);
-            while(first.hp > 0 || second.hp > 0){
-            if(random< 50){    
-
             first.hp -= (second.forta) / first.protection;
-            }
-            
+        }
 
-            else{
+        
 
-            second.hp -= (first.forta) / second.protection;
-            }
-            if(first.hp >= 0){
-            Winner = first;
-            }
 
-            else{
-            Winner = second;
-            }
 
-}
 
-    return "Winner is" + Winner.name;
     }
-}
 }
